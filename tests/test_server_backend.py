f1007a7118ea14b10e0d21aa996f2c68f341cf36
Paulo Nascimento <paulormnas@gmail.com>
2021-06-15 22:37:55 -0300

    Adiciona HTTPStatus ao testes

--- tests/test_server_backend.py
+++ tests/test_server_backend.py
@@ -3,6 +3,7 @@ import unittest
 import json
 from backend.server_app import create_app
 from flask import request
+from http import HTTPStatus
 from os import path
 from backend.security.Sign import Signature
 
@@ -36,15 +37,14 @@ class AppTest(unittest.TestCase):
         # Utiliza contexto de cliente para uma requisiçao GET na raiz (/) e confere as respostas do serviço
         with self.app.test_client() as client:
             response = client.get('/')
-            self.assertTrue(response.status_code == 200)
-            # self.assertEqual(b'Server funcionando', response.data)
+            self.assertEqual(HTTPStatus.OK, response.status_code)
 
     def test_de_envio_de_dados_na_rota_registrar_dados(self):
         # Verifica se a quantidade de arquivos na pasta "registros" esta maior apos o envio de dados via POST
         quantidade_inicial_arquivos = len(os.listdir(self.registers_path))
         with self.app.test_client() as client:
             response = client.post('/registrar_dados', data=json.dumps(self.dados_de_teste))
-            self.assertEqual(200, response.status_code)
+            self.assertEqual(HTTPStatus.OK, response.status_code)
 
         quantidade_final_arquivos = len(os.listdir(self.registers_path))
         self.assertGreater(quantidade_final_arquivos, quantidade_inicial_arquivos)
@@ -54,14 +54,14 @@ class AppTest(unittest.TestCase):
 
         with self.app.test_client() as client:
             response = client.get('/gerar_chaves/')
-            self.assertEqual(404, response.status_code)
+            self.assertEqual(HTTPStatus.NOT_FOUND, response.status_code)
 
     def test_gerar_chaves_com_id_do_dispositivo(self):
         # Testa rota de geraçao de chaves informando id do dispositivo
 
         with self.app.test_client() as client:
             response = client.get('/gerar_chaves/{}'.format(self.std_device_id))
-            self.assertEqual(200, response.status_code)
+            self.assertEqual(HTTPStatus.OK, response.status_code)
             keys_json = response.data
             keys = json.loads(keys_json)
             self.assertEqual(2, len(keys))
@@ -74,7 +74,7 @@ class AppTest(unittest.TestCase):
             print(dados)
             response = client.get('/verifica_dispositivo?device_id={}&data={}'.format(self.std_device_id, dados),
                                   data=assinatura)
-            self.assertEqual(200, response.status_code)
+            self.assertEqual(HTTPStatus.OK, response.status_code)
             verification_response = response.data.decode("utf-8")
             self.assertEqual("valid", verification_response)
 
@@ -85,17 +85,16 @@ class AppTest(unittest.TestCase):
             dados = json.dumps(self.dados_de_teste)
             response = client.get('/verifica_dispositivo?device_id=SiMon-meter&data={}'.format(dados),
                                   data=assinatura)
-            self.assertEqual(200, response.status_code)
+            self.assertEqual(HTTPStatus.OK, response.status_code)
             verification_response = response.data.decode("utf-8")
             self.assertEqual("invalid", verification_response)
 
     def test_assinatura(self):
         # Testa rota de assinar dados
-
         with self.app.test_client() as client:
             dados = json.dumps(self.dados_de_teste)
             response = client.get('/sign', data=dados)
-            self.assertEqual(200, response.status_code)
+            self.assertEqual(HTTPStatus.OK, response.status_code)
             signed_data = response.data
             assinatura = self.dados_de_teste_assinados["signature"]
             self.assertEqual(assinatura, signed_data)
