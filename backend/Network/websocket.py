6a7259e32a0260d5f6be252369634af96741045f
Paulo Nascimento <paulormnas@gmail.com>
2022-08-12 00:29:45 -0300

    feat: organiza servidor websocket pra rodar em paralelo com servidor HTTP

--- backend/Network/websocket.py
+++ backend/Network/websocket.py
@@ -1,62 +1,68 @@
 import asyncio
 import websockets
 import json
-
-standard = None
-clients = set()
+from threading import Thread
 
 
-async def handler(websocket):
-    global clients
-    global standard
+class WebsocketServer(Thread):
+    def __init__(self, host="0.0.0.0", port=8000):
+        Thread.__init__(self)
+        self.standard = None
+        self.clients = set()
+        self.websocket_host = host
+        self.port = port
 
-    while True:
-        try:
-            message_json = await websocket.recv()
-            message = json.loads(message_json)
-            if message["device"] == "standard":
-                standard = websocket
+    async def handler(self, websocket):
+        while True:
+            try:
+                message_json = await websocket.recv()
+                message = json.loads(message_json)
+                if message["device"] == "standard" and websocket != self.standard:
+                    self.standard = websocket
+                    print("[WEBSOCKET-SERVER] Standard conectado")
 
-            if message["device"] == "client":
-                clients.add(websocket)
+                if message["device"] == "client" and websocket not in self.clients:
+                    self.clients.add(websocket)
+                    print("[WEBSOCKET-SERVER] Client conectado")
 
-            if websocket in clients:
-                await handle_message_to_standard(message_json)
-                print("[CLIENT]", message["cmd"])
+                if websocket in self.clients:
+                    await self.handle_message_to_standard(message_json)
+                    print("[WEBSOCKET-CLIENT]", message["cmd"])
 
-            if websocket == standard:
-                print("[STANDARD]", message["cmd"])
-                await handle_message_to_clients(message["cmd"])
+                if websocket == self.standard:
+                    print("[WEBSOCKET-STANDARD]", message["cmd"])
+                    await self.handle_message_to_clients(message["cmd"])
 
-            payload = {"cmd": "ACK"}
-            await websocket.send(json.dumps(payload))
-        except(websockets.exceptions.ConnectionClosedOK,
-               websockets.exceptions.ConnectionClosedError):
-            if websocket == standard:
-                print("[SERVER] Standard desconectado")
-                standard = None
-            if websocket in clients:
-                print("[SERVER] Client desconectado")
-                clients.remove(websocket)
+                payload = {"cmd": "ACK"}
+                await websocket.send(json.dumps(payload))
+            except (
+                websockets.exceptions.ConnectionClosedOK,
+                websockets.exceptions.ConnectionClosedError,
+            ):
+                if websocket == self.standard:
+                    print("[WEBSOCKET-SERVER] Standard desconectado")
+                    self.standard = None
+                if websocket in self.clients:
+                    print("[WEBSOCKET-SERVER] Client desconectado")
+                    self.clients.remove(websocket)
 
+    async def handle_message_to_standard(self, message):
+        if self.standard is not None:
+            await self.standard.send(message)
+        else:
+            websockets.broadcast(self.clients, "Standard desconectado")
 
-async def handle_message_to_standard(message):
-    global standard
-    if standard is not None:
-        await standard.send(message)
-    else:
-        websockets.broadcast(clients, "Standard desconectado")
+    async def handle_message_to_clients(self, message):
+        websockets.broadcast(self.clients, message)
 
+    async def main(self):
+        async with websockets.serve(self.handler, self.websocket_host, self.port):
+            await asyncio.Future()  # run forever
 
-async def handle_message_to_clients(message):
-    global clients
-    websockets.broadcast(clients, message)
-
-
-async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8000):
-        await asyncio.Future()  # run forever
+    def run(self):
+        asyncio.run(self.main())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    t = WebsocketServer()
+    t.start()
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import sys
+from backend.Network.websocket import WebsocketServer
 from backend.server_app import create_app
+
+ws = WebsocketServer()
+ws.start()
 
 _mode = sys.argv[1] if len(sys.argv) > 1 else "development"
 app = create_app(mode=_mode)
